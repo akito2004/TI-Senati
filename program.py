8ee010aabbe95fbfba5d5aa0abecc285bda70a5a
akito2004 <138918940+akito2004@users.noreply.github.com>
2023-07-07 15:34:08 -0500

    Add files via upload

---
+++ program.py
@@ -0,0 +1,43 @@
+
+
+def cant_vc(n):
+   cantidad_vocales=0
+   cantidad_consonates=0
+   for letra in n:
+      if letra=="a" or letra=="i or letra=="0"
+            cantidad_vocales+= 1
+      else:
+          cantidad_consonates+= 1
+    r=f"""nombre: {n.capitalize()}
+        numero de vocales:{cantidad_vocales}
+        numero de consonantes:{cantidad_consonantes}
+        numero de letras.{len(n)}"""
+    return r
+    print(cant_vc("juan"))
+
+
+
+
+
+
+lista_nombres=[]
+flag True
+while flag:
+
+    nombres = input ("ingrese nombre o para terminar  escriba x:")
+    if nombres =="x":
+      code  in list_nombres 
+      flag=False
+    else:
+        c=nombres.capitalize()
+        if nombre in list_nombres:
+            pass
+        else:
+            lista_nombres .append(cnombres )
+
+lista_nombre.short()
+print(lista_nombres)
+print(f"la cantida de nombres  son {len(lista_nombres)}")  
+
+for i in lista_nombres:
+    print(cant_vc{1})
