2e290121b3bf1794f63d36a7dd0b2427a769817c
akito2004 <138918940+akito2004@users.noreply.github.com>
2023-07-14 14:25:20 -0500

    Add files via upload

---
+++ while.py
@@ -0,0 +1,10 @@
+flag=True
+while flag: # esto da vueltas
+    try:    # captura errores
+            # parte 1 que si todo sale bien
+        numero = int(input('Ingrese solo numeros: '))
+        print(numero)
+        flag=False
+    except ValueError as e:
+             #que si en la parte1 hubo un error imprime esto de aqui
+        print("usted escribio una letra")
