aedade7609662bafdd2fbfb532545e6bc9a04160
akito2004 <138918940+akito2004@users.noreply.github.com>
2023-08-04 14:13:27 -0500

    Add files via upload

---
+++ operacion.py
@@ -0,0 +1,11 @@
+from productos import Producto
+
+
+
+lista_productos  =   []
+# print(lista_productos)
+pproducto=Producto("1","1",1, 1)
+print(pproducto.info())
+
+nombre_p    =input("nombe del producto: ")
+marca_p     =input("marca del producto: ")
---
+++ productos.py
@@ -0,0 +1,10 @@
+class  Producto:
+    def __init__(self,nombre,marca,precio,cantidad):
+        self.nombre=nombre
+        self.marca = marca
+        self.precio=precio
+        self.cantidad=cantidad
+    def info(self):
+        return  f"{self.nombre}{self.marca}{self.cantidad}"
+    def actualizar_cantidad(self,cantidad_vendida):
+        self.cantidad =self.cantidad - cantidad_vendida
