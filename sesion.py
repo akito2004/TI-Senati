5aa127de236dc0a710f4bb4d3f81556dd9086275
akito2004 <138918940+akito2004@users.noreply.github.com>
2024-03-20 13:45:59 -0500

    Add files via upload

---
+++ sesion.py
@@ -0,0 +1,71 @@
+""" a = 15
+
+ b = 30
+
+ c = a + b
+
+print (c)"""
+
+
+
+
+"""x = 10
+y = 5
+z = x-y
+print("la resta es:",z)"""
+
+
+
+"""a =10
+b =12
+c =15
+suma = a+b+c
+resta =a-b-c
+multiplicacion=a*b*c
+print("la respuetas son: ",suma,resta,multiplicacion)"""
+
+"""a = int(input("ingrese un numero: " ))
+b = int(input("ingrese otro numero: "))
+c = a-b
+print("la resta es :",c)
+ 
+d = int(input("ingrese un numero"))
+e = int(input("ingrese un nuevo valor"))
+f = d*e
+print("la muitiplicacion es:",f)
+
+g =int(input("ingrese un numero"))
+h =int(input("ingrese un nuevo valor"))
+i =g/h
+print("la divicion es:",i)"""
+
+
+"""a = int(input("ingrese un numero"))
+b = a*a
+print("el vaor al cuadrado es:",b)"""
+
+"""a = int(input("ingresar el valor de la base"))
+b = int(input("ingrese el valor de la altura"))
+
+c = a*b/2
+
+print(c)"""
+
+"""a =input("cual es tu nombre: ")
+print("bienvenido a senati:",a)"""
+
+"""realiza  un programa  que muestre el resultado total de  un montono de dinero asignado al hospital dependiendo de los siguientes porcentajers
+
+primero = 25%    0.25
+segunhdo = 35%    0.35
+tercero = 40%     0.40
+"""
+a = int(input("ingrese un monto: "))
+
+p1=a*0.25
+p2=a*0.35
+p3=a*0.40
+
+print(p1)
+print(p2)
+print(p3)
