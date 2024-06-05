0a5c44ff9ce6b4e5817a09d1bc7b64fe164108a4
akito2004 <138918940+akito2004@users.noreply.github.com>
2024-06-05 16:11:15 -0500

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,17 @@
+create DataBase  Estudiantes_ primaria
+
+create table Estudiantes_primaria(
+
+id INT,
+Nombre  varchar (50),
+nacionalidad   varchar  (50),
+correo VARCHAR (50),
+telefono VARCHAR(15)
+
+);
+INSERT  INTO  ATRIBUTOS ('1','juan','mexicana','',' telefono',' )1j'
+INSERT   INTO ATRIBUTOS  (id,nombre, nacionalidad, correo, telefono, values)2
+INSERT    INTO  ATRIBUTOS (id,nombre,  nacionalidad, correo  telefono, values)3
+Insert    INTO   ATRIBUTOS (id  nombre, nacionalidad, correo, telefono  VALUES)4
+
+SELECT * FROM Estudiantes_primaria
